2175469aacdde03e9757a938be9064727675f261
Tobias Laufkötter <tobias.laufkoetter@gmail.com>
2020-04-03 18:43:15 +0200

    make app accessible from the outside

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace BudgetierWeb
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("https://0.0.0.0:6001", "http://0.0.0.0:6000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
